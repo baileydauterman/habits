ce35a6714e69e4998fe16829d9129e62ed042fa6
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2024-01-07 21:25:18 -0500

    Add project files.

---
+++ src/Habits/Habits/Data/Goal.cs
@@ -0,0 +1,36 @@
+﻿namespace Habits.Data
+{
+    public class Goal
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public Interval Interval { get; set; } = Interval.Weekly;
+
+        public Measurement Measurement { get; set; }
+
+        public double GoalValue { get; set; } = 0;
+
+        public double PercentProgress => TotalProgress / GoalValue * 100;
+
+        public double TotalProgress => ProgressCheckins.Select(x => x.Progress).Sum();
+
+        public List<ProgressCheckin> ProgressCheckins { get; set; } = new List<ProgressCheckin>();
+    }
+
+    public enum Interval
+    {
+        None,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly
+    }
+
+    public enum Measurement
+    {
+        lbs,
+        miles,
+    }
+}
---
+++ src/Habits/Habits/Data/ProgressCheckin.cs
@@ -0,0 +1,9 @@
+﻿namespace Habits.Data
+{
+    public class ProgressCheckin
+    {
+        public DateTime Date { get; set; }
+
+        public double Progress { get; set; }
+    }
+}
